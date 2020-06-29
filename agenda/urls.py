db8293d03501f4d003a7fa7cfe397da9a74b999d
FerLK <luiz.citu@gmail.com>
2020-06-29 15:56:44 -0300

    Incluido template para inserção de eventos da agenda

--- agenda/urls.py
+++ agenda/urls.py
@@ -21,7 +21,10 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
     path('',views.index),
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
     path('logout/', views.logout_user)
--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
+
 # Create your views here.
 def index(request):
     return redirect('/agenda/')
@@ -32,3 +33,30 @@ def lista_eventos(request):
     evento = Evento.objects.filter(usuario = usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
+
+
+@login_required(login_url = '/login/')
+def evento(request):
+    return render(request,'evento.html')
+
+
+@login_required(login_url = '/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo = titulo,
+                              data_evento = data_evento,
+                              descricao = descricao,
+                              usuario = usuario)
+        return redirect('/')
+
+    return  redirect('/')
+
+
+@login_required(login_url = '/login/')
+def delete_evento(request,id_evento):
+    Evento.objects.filter(id=id_evento).delete()
+    return redirect('/')
