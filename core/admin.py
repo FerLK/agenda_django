51ae1f00abbef74425106412c3527fd8f9bceaf2
FerLK <luiz.citu@gmail.com>
2020-06-30 16:19:18 -0300

    adicionado funções de alterações e exclusão de dados

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+#mostra as características do "Evento" (classe model) no setup
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('id','titulo','data_evento','data_criacao')
+    list_filter = ('titulo','data_evento',)#filter recebe uma tupla, se for item unico colocar virgula!
+
+
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -6,14 +6,12 @@ from django.contrib.auth.models import User
 class Evento(models.Model):
     titulo = models.CharField(max_length = 100)
     descricao = models.TextField(blank = True, null = True)
-    data_evento = models.DateTimeField(verbose_name='Data do Evento') #verboso name serve para customizar os nomes...
-    #... do DjangoAdmin
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateField(auto_now = True,verbose_name='Data da criação')
-    #chave estrangeira para multusuário
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)  # .cascade - se o usuario for excluido, todos os...
-    #... eventos dele desaparecem
 
-    #serve para dar um nome a tabela que será criada
+
+
     class Meta:
         db_table = 'evento'
     #retorna o titulo do evento no setup (senao ficará apenas a referência a um objeto)
@@ -22,4 +20,7 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
 
